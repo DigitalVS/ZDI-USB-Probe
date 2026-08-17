bf07ff880fcc70158162c7b16eb3d0857b8ea046
Vitomir Spasojević <vitomir.spasojevic@gmail.com>
2026-08-17 13:23:13 +0200

    Version 0.2.0

--- zdiusb/src/cmd_version.cpp
+++ zdiusb/src/cmd_version.cpp
@@ -14,7 +14,7 @@ ResponseBuf CmdVersion::getResponse() {
   // Create a response message
   Cmd::outBuffer[0] = VERSION; // Message type
   Cmd::outBuffer[1] = 0; // Major version number
-  Cmd::outBuffer[2] = 1; // Minor version number
+  Cmd::outBuffer[2] = 2; // Minor version number
   Cmd::outBuffer[3] = 0; // Revision number
 
   return ResponseBuf { .startAddr = Cmd::outBuffer, .size = 4 };
